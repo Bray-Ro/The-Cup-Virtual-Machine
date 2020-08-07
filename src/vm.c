969ed973fa8d253c9d2b3f5f5a7b661d5c1c94c7
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2020-08-07 14:42:06 -0400

    Update vm.c

--- src/vm.c
+++ src/vm.c
@@ -6,6 +6,14 @@ The Cup Virtual Machine Alpha
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <assert.h>
+
+struct STACK_STRUCT {
+
+    int STACK[5000];
+};
+
 char* read_file(char fname[5000]) {
         /* declare a file pointer */
     FILE    *fptr;
@@ -30,26 +38,77 @@ char* read_file(char fname[5000]) {
     fclose (fptr);
     return file;
 }
-int byte;
+void chopN(char *str, size_t n)
+{
+    assert(n != 0 && str != 0);
+    size_t len = strlen(str);
+    if (n > len)
+        return;  // Or: n = len;
+    memmove(str, str+n, len - n + 1);
+}
+int NEXT_BYTE(char program[5000]) {
+        unsigned int byte;
+        char prog_backup[5000];
+        int cpyi;
+        for(cpyi=0; program[cpyi]!='\0'; cpyi++) {
+            prog_backup[cpyi] = program[cpyi];
+        }
 
+        char strbyte[5000];
+        strcpy(strbyte, strtok(prog_backup, " "));
+        
+        int i;
+        int byte_len = strlen(strbyte);
+        
+        // for(i=0; i<byte_len; i++){
+
+        //     byte = byte * 10 + ( strbyte[i] - '0' );
+
+        // }
+        byte = atoi(strbyte);
+        printf("\nBYTETIME:%d\n", byte);
+        return byte;
+}
 int main(int argc, char *argv[]) {
+    /* Create data stack*/
+    struct STACK_STRUCT DATA_STACK;
+ 
+    int DATA_STACK_SIZE = 0;
+      
+    /* byte stores current opcode */
+    unsigned int byte;
+    /* get contents of a cup bytecode file */
     char *buffer = read_file(argv[1]);
     char program[5000];
     strcpy(program, buffer);
-    char prog_backup[5000];
-    int cpyi;
-    for(cpyi=0; program[cpyi]!='\0'; cpyi++) {
-        prog_backup[cpyi] = program[cpyi];
-    }
+    sprintf(program, "%s ", program);
+    /* Execute program*/
+    int BYTE_ADDR = 0;
+    while (BYTE_ADDR < strlen(program)) {
+        
+        byte = NEXT_BYTE(program);
+        
+        switch (byte) {
+            case 1:
+                 /* PUSH Instruction */
+                chopN(program, 2);
+                byte = 0;
+           
+                byte = NEXT_BYTE(program);
+                
+                
+                char BYTE_STR[5000];
+                sprintf(BYTE_STR, "%d", byte);
+                chopN(program, strlen(BYTE_STR));
+                DATA_STACK.STACK[DATA_STACK_SIZE] = byte; 
+             
+                DATA_STACK_SIZE++;
+                
 
-    char strbyte[5000];
-    strcpy(strbyte, strtok(prog_backup, " "));
-    int i;
-    int byte_len = strlen(strbyte);
-    for(i=0; i<byte_len; i++){
-
-        byte = byte * 10 + ( strbyte[i] - '0' );
-
+                break;
+        }
+    
+        BYTE_ADDR++;
     }
     return 0;
 }
