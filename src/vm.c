682b10b24731c7bec4248d6bbe26d9ba40d2fe8d
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2020-08-11 21:21:06 -0400

    Update vm.c

--- src/vm.c
+++ src/vm.c
@@ -1,6 +1,6 @@
 /*
-#############################
-The Cup Virtual Machine Alpha
+##################################
+# The Cup Virtual Machine Alpha  #
 ##################################
 */
 #include <stdio.h> 
@@ -8,9 +8,12 @@ The Cup Virtual Machine Alpha
 #include <string.h>
 #include <ctype.h>
 #include <assert.h>
-
+/* Define Opcodes*/
+#define MOVE 1
+#define ADD 2
+#define CONDITIONAL 3
 int registers[32000];
-
+int program_arr[32000];
 char* read_file(char fname[5000]) {
         /* declare a file pointer */
     FILE    *fptr;
@@ -83,33 +86,95 @@ int main(int argc, char *argv[]) {
     /* Execute program*/
     int BYTE_ADDR = 0;
     while (BYTE_ADDR < strlen(program)) {
-        
+        printf("\nprog:%s\n", program);
         byte = NEXT_BYTE(program);
-        
+        program_arr[BYTE_ADDR] = byte;
+        char BYTE_STR[5000];
         switch (byte) {
-            case 1:
+            case MOVE:
+                printf("\nprog:%s\n", program);
                  /* MOVE Instruction */
                 chopN(program, 2);
+                printf("\nprog:%s\n", program);
                 byte = 0;
+                
 
-                char BYTE_STR[5000];
-                int ADDR = NEXT_BYTE(program);
-                sprintf(BYTE_STR, "%d", ADDR);
-                chopN(program, strlen(BYTE_STR));
+                
 
+                /* Get Byte to move to the address */
                 byte = NEXT_BYTE(program);
                 sprintf(BYTE_STR, "%d", byte);
-                chopN(program, strlen(BYTE_STR));
-                printf("\nREGISTER:%d\n", registers[ADDR]);
+                
+                chopN(program, strlen(BYTE_STR)+1);
+                /* Get Adress of register to move to */
+                int ADDR = NEXT_BYTE(program);
+                sprintf(BYTE_STR, "%d", ADDR);
+                chopN(program, strlen(BYTE_STR)+1);
+            
+                printf("\nADDR:%d VAL:%d prog:%s\n", ADDR, registers[ADDR], program);
+                /* Move byte to Register*/
                 registers[ADDR] = byte; 
-                printf("\nREGISTER:%d\n", registers[ADDR]);
+                printf("\nADDR:%d VAL:%d prog:%s\n", ADDR, registers[ADDR], program);
                 amount_registers++;
                 
 
                 break;
-                 
+            case ADD:
+                /* ADD instruction */
+                chopN(program, 2);
+                byte = 0;
+
+             
+              
+                int REG_1_ADDR = NEXT_BYTE(program);
+                sprintf(BYTE_STR, "%d", REG_1_ADDR);
+                chopN(program, strlen(BYTE_STR)+1);      
+      
+                int REG_2_ADDR = NEXT_BYTE(program);
+                sprintf(BYTE_STR, "%d", REG_2_ADDR);
+                chopN(program, strlen(BYTE_STR)+1);
+        
+                int RESULT_ADDR = NEXT_BYTE(program);
+                sprintf(BYTE_STR, "%d", RESULT_ADDR);
+                chopN(program, strlen(BYTE_STR)+1);
+                registers[RESULT_ADDR] = registers[REG_1_ADDR] + registers[REG_2_ADDR]; 
+            case CONDITIONAL:
+                /* CONDITIONAL instruction */
+                chopN(program, 2);
+                byte = 0;
+
+                int CONDITIONAL_TYPE = NEXT_BYTE(program);
+                sprintf(BYTE_STR, "%d", CONDITIONAL_TYPE);
+                chopN(program, strlen(BYTE_STR));
+
+                /* Get adress of registers to compare */
+                int REG_1_ADDR = NEXT_BYTE(program);
+                sprintf(BYTE_STR, "%d", REG_1_ADDR);
+                chopN(program, strlen(BYTE_STR));
+
+                int REG_2_ADDR = NEXT_BYTE(program);
+                sprintf(BYTE_STR, "%d", REG_2_ADDR);
+                chopN(program, strlen(BYTE_STR));
+
+                /* Get instruction index to jump to if conditional is true*/
+                int JUMP = NEXT_BYTE(program);
+                sprintf(BYTE_STR, "%d", JUMP);
+                chopN(program, strlen(BYTE_STR));
+
+                /* Get Value of registers to compare */
+                int REG_1_VALUE = registers[REG_1_ADDR];
+                int REG_2_VALUE = registers[REG_2_ADDR];
+                switch (CONDITIONAL) {
+                    case 1:
+                        /* == */ 
+                        if (REG_1_VALUE == REG_2_VALUE) {
+                            
+                        }
+
+                }
+
         }
-     
+        printf("\nprog:%s\n", program);
         BYTE_ADDR++;
     }
     return 0;
